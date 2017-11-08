9c20c915495e86bbb6d02b6b2ee75cbb8751eda2
Hu Shaohua <Hu Shaohua@192.168.1.110>
2017-11-08 16:55:41 +0800

    commit again

---
+++ src/hsh/newtest_3/commitagain.java
@@ -0,0 +1,10 @@
+package hsh.newtest_3;
+
+public class commitagain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("can you get this?");
+	}
+
+}
