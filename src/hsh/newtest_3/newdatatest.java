b9358fa8f43b8178d172946f3b4d1790fab9113b
hushaohua <1784937336@qq.com>
2017-11-13 14:41:24 +0800

    打印 newdatatest

---
+++ src/hsh/newtest_3/newdatatest.java
@@ -0,0 +1,14 @@
+package hsh.newtest_3;
+
+public class newdatatest {
+	
+ int a=10,b=20;
+ int temp;
+ public void change(){
+	 temp=a;
+	 a=b;
+	 b=temp;
+	 System.out.println("a="+a+"b="+b);
+ }
+
+}
